2ef0c2977d99a9c516649157bfe48af02251a019
gspezi15 <75540750+gspezi15@users.noreply.github.com>
2022-08-27 22:45:19 +0100

    Starter content

---
+++ luna.lua
@@ -0,0 +1,6 @@
+local areaNames = require("areaNames")
+local warpTransition = require ("warpTransition")
+local littleDialogue = require("littleDialogue")
+local smwMap = require("smwMap")
+littleDialogue.defaultStyleName = "ml"
+local extraSounds = require ("extrasounds")
